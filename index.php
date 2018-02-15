745c1211e35d4ff5f9664762230a3b9411eef95c
Vanyafer <33906074+Vanyafer@users.noreply.github.com>
2018-02-14 23:57:45 -0600

    x

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <title>MooCouture</title>
+  
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="orden.css">
+    <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body  >
+
+<nav>
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                      
+      </button>
+    </div>
+  <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-left">
+        <li><a href="">Registrarse</a></li>
+        <li><a href="">Sesion</a></li>
+        <li class="esconder"><a href="#">Inicio</a></li>
+        <li class="esconder">
+          <a href="#">Ofertas</a>
+        </li> 
+        <li class="esconder">
+          <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+            Ropa
+          </a>
+          <div class="dropdown-menu">
+             <ul>
+                <li><a class="dropdown-item" href="#">Blusas</a></li>
+                <li><a class="dropdown-item" href="#">Vestidos</a></li>
+                <li><a class="dropdown-item" href="#">Pantalones</a></li>
+                <li><a class="dropdown-item" href="#">Faldas</a></li>
+              </ul>
+          </div>
+        </li>
+        <li class="esconder">
+          <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+           Accesorios
+          </a>
+          <div class="dropdown-menu">
+              <ul>
+                <li><a class="dropdown-item" href="#">Collares</a></li>
+                <li><a class="dropdown-item" href="#">Lentes</a></li>
+                <li><a class="dropdown-item" href="#">Bisuteria</a></li>
+                <li><a class="dropdown-item" href="#">Bolsos</a></li>
+              </ul>
+          </div>
+        </li>
+
+      </ul>
+
+      <form class="navbar-form navbar-right">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search">
+            <div class="input-group-btn">
+              <button class="btn btn-default" type="submit">
+                <i class="glyphicon glyphicon-search"></i>
+              </button>
+            </div>
+          </div>
+        </form>
+    </div>
+</nav>
+
+<img src="nombre.jpg" class="logo">
+
+<nav class="navbar">
+ 
+      <ul class="nav navbar-nav">
+        <li class="nav-item"><a href="#">Inicio</a></li>
+        <li class="nav-item">
+          <a href="#">Ofertas</a>
+        </li> 
+        <li class="nav-item">
+          <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+            Ropa
+          </a>
+          <div class="dropdown-menu">
+             <ul>
+                <li><a class="dropdown-item" href="#">Blusas</a></li>
+                <li><a class="dropdown-item" href="#">Vestidos</a></li>
+                <li><a class="dropdown-item" href="#">Pantalones</a></li>
+                <li><a class="dropdown-item" href="#">Faldas</a></li>
+              </ul>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+           Accesorios
+          </a>
+          <div class="dropdown-menu">
+              <ul>
+                <li><a class="dropdown-item" href="#">Collares</a></li>
+                <li><a class="dropdown-item" href="#">Lentes</a></li>
+                <li><a class="dropdown-item" href="#">Bisuteria</a></li>
+                <li><a class="dropdown-item" href="#">Bolsos</a></li>
+              </ul>
+          </div>
+        </li>
+      </ul>
+
+</nav>
+<div class="galeria">
+  <div class="imagen">
+      <a href=""> <img src="img/falda.jpg"></a>
+      <div class="desc"><p>Color</p><p>Precio</p></div>
+  </div>
+</div>
+<div class="galeria">
+  <div class="imagen">
+      <a href=""><img src="img/lentes4.jpg"></a>
+      <div class="desc"><p>Color</p><p>Precio</p></div>
+  </div>
+</div>
+<div class="galeria">
+  <div class="imagen">
+      <a href=""><img src="img/pantalon3.jpg"> </a>
+      <div class="desc"><p>Color</p><p>Precio</p></div>
+  </div>
+</div>
+<div class="galeria">
+  <div class="imagen">
+      <a href=""> <img src="img/falda3.jpg"></a>
+      <div class="desc"><p>Color</p><p>Precio</p></div>
+  </div>
+</div>
+<div class="galeria">
+  <div class="imagen">
+      <a href=""><img src="img/pantalon4.jpg"></a>
+      <div class="desc"><p>Color</p><p>Precio</p></div>
+  </div>
+</div>
+<div class="galeria">
+  <div class="imagen">
+      <a href=""><img src="img/collar1.jpg"> </a>
+      <div class="desc"><p>Color</p><p>Precio</p></div>
+  </div>
+</div>
+<div class="galeria">
+  <div class="imagen">
+      <a href=""> <img src="img/blusa2.jpg"></a>
+      <div class="desc"><p>Color</p><p>Precio</p></div>
+  </div>
+</div>
+<div class="galeria">
+  <div class="imagen">
+      <a href=""><img src="img/blusa4.png"></a>
+      <div class="desc"><p>Color</p><p>Precio</p></div>
+  </div>
+</div>
+
+<footer>
+<p>Contacto: Correo: MooCouture@gmail.com Telefono: 3333333333 </p>
+</footer>
+
+</body>
+</html>
